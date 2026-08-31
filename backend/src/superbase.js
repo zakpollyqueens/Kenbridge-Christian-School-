e72bffbfb39310bdede6834156d66c884c4dfbff
Zakmolanitech <zakpollyqueens@gmail.com>
2026-08-31 10:26:27 +0300

    Add Supabase server authentication client

---
+++ backend/src/superbase.js
@@ -0,0 +1,15 @@
+const { createClient } = require("@supabase/supabase-js");
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SECRET_KEY,
+  {
+    auth: {
+      autoRefreshToken: false,
+      persistSession: false,
+      detectSessionInUrl: false
+    }
+  }
+);
+
+module.exports = supabase;
